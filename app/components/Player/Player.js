0c386afce91855de7ccda71bde7ea71b3b8065de
Tomas Acin <acinched@usc.edu>
2019-03-01 15:35:13 -0800

    re-added buttons

--- app/components/Player/Player.js
+++ app/components/Player/Player.js
@@ -6,12 +6,14 @@ import { Link } from 'react-router-dom';
 // import routes from '../../constants/routes';
 import styles from './Player.css';
 
-
-type Props = {};
-
-export default class Player extends Component<Props> {
-  props: Props;
-  wavesurfer;
+export default class Player extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playing: true,
+    };
+    this.wavesurfer;
+  }
 
   componentDidMount() {
     this.wavesurfer = WaveSurfer.create({
@@ -19,11 +21,48 @@ export default class Player extends Component<Props> {
       barWidth: 2,
       height: 80
     });
+    this.wavesurfer.load('http://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3')
   }
 
   loadWaveForm = () => {
     console.log("loading in waveform")
     this.wavesurfer.load('http://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3')
+  }
+
+  play = () => {
+    console.log("in play");
+    this.wavesurfer.playPause();
+    var play_btn = document.getElementById('play-button');
+    if (this.state.playing) {
+      play_btn.style.backgroundColor = 'green';
+    }
+    else {
+      play_btn.style.backgroundColor = 'yellow';
+    }
+    this.setState({
+      playing: !this.state.playing
+    });
+  }
+
+  previous = () => {
+
+  }
+
+  restart = () => {
+    console.log('in restart');
+    this.wavesurfer.stop();
+    if (!this.state.playing)
+    {
+      this.setState({
+        playing: !this.state.playing
+      });
+      var play_btn = document.getElementById('play-button');
+      play_btn.style.backgroundColor = 'yellow';
+    }
+  }
+
+  next = () => {
+
   }
 
   render() {
@@ -32,6 +71,10 @@ export default class Player extends Component<Props> {
         <p>Player goes here!</p>
         <a onClick={this.loadWaveForm}>Load Waveform</a>
         <div className={styles.waveform} id='waveform'></div>
+        <button type="button" onClick={this.previous} id="previous-button">Previous</button>
+        <button type="button" onClick={this.restart} id="restart-button">Restart</button>
+        <button type="button" onClick={this.play} id="play-button">Play/Pause</button>
+        <button type="button" onClick={this.next} id="next-button">Next</button>
       </div>
     );
   }
