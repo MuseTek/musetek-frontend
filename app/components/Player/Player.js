26703c0dfe07d9a69b016e772fa8aa6eec7a7b34
Tomas Acin <acinched@usc.edu>
2019-02-28 21:36:21 -0800

    added buttons; trying out data package

--- app/components/Player/Player.js
+++ app/components/Player/Player.js
@@ -6,12 +6,14 @@ import { Link } from 'react-router-dom';
 // import routes from '../../constants/routes';
 import styles from './Player.css';
 
-
-type Props = {};
-
-export default class Player extends Component<Props> {
-  props: Props;
-  wavesurfer;
+export default class Player extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        playing: true,
+      };
+      this.wavesurfer;
+    }
 
   componentDidMount() {
     this.wavesurfer = WaveSurfer.create({
@@ -23,8 +25,44 @@ export default class Player extends Component<Props> {
   }
 
   loadWaveForm = () => {
-    console.log("loading in waveform")
+    console.log("loading in waveform");
     this.wavesurfer.load('http://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3')
+  }
+
+  play = () => {
+    console.log("in play");
+    this.wavesurfer.playPause();
+    var play_btn = document.getElementById('play-button');
+    if (this.state.playing) {
+      play_btn.style.backgroundColor = 'green';
+    }
+    else {
+      play_btn.style.backgroundColor = 'yellow';
+    }
+    this.setState({
+      playing: !this.state.playing
+    });
+  }
+
+  previous = () => {
+
+  }
+
+  restart = () => {
+    console.log('in restart');
+    this.wavesurfer.stop();
+    if (!this.state.playing)
+    {
+      this.setState({
+        playing: !this.state.playing
+      });
+      var play_btn = document.getElementById('play-button');
+      play_btn.style.backgroundColor = 'yellow';
+    }
+  }
+
+  next = () => {
+
   }
 
   render() {
@@ -34,6 +72,10 @@ export default class Player extends Component<Props> {
         {/* <a onClick={this.loadWaveForm}>Load Waveform</a> */}
         {/* <img src='' */}
         <div className={styles.waveform} id='waveform'></div>
+        <button type="button" onClick={this.previous} id="previous-button">Previous</button>
+        <button type="button" onClick={this.restart} id="restart-button">Restart</button>
+        <button type="button" onClick={this.play} id="play-button">Play/Pause</button>
+        <button type="button" onClick={this.next} id="next-button">Next</button>
       </div>
     );
   }
